015315bcf4570ad15da294fa4b3c7d8bfddcaa29
Dmitry <31991163+dassinion@users.noreply.github.com>
2017-12-19 11:19:50 +0300

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -20,17 +20,17 @@ Handle<Value> except(const char* msg) {
 
 void Scrypt(const v8::FunctionCallbackInfo<v8::Value>& args) {
    v8::Isolate* isolate = args.GetIsolate();
-   v8::HandleScope scope(isolate);
+//   v8::HandleScope scope(isolate);
 
    if (args.Length() < 3) {
-       except("You must provide buffer to hash, N value, and R value");
+       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
        return;
    }
 
    Local<Object> target = args[0]->ToObject();
 
    if(!Buffer::HasInstance(target)) {
-       except("Argument should be a buffer object.");
+       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate,"Argument should be a buffer object.")));
        return;
    }
 
